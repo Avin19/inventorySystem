2f188b593b27c01e5a9f405cb8170fb000c0a0e1
Avin19 <avinashy1993@gmail.com>
2024-04-28 06:54:50 +0530

    Started Working on the player Status

---
+++ Assets/Scripts/SO/SOcreater/PlayerStatus.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStatus : ScriptableObject
+{
+    public float playerItemWeight;
+    public float maxplayerWeight;
+
+}
