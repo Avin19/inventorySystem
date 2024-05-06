328dfb77bb0745cf6a7b85494c6b370a946db793
Avin19 <avinashy1993@gmail.com>
2024-05-06 11:19:42 +0530

    Working on Review

--- Assets/Scripts/SO/SOcreater/PlayerStatus.cs
+++ Assets/Scripts/SO/SOcreater/PlayerStatus.cs
@@ -1,10 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "PlayerStatus", menuName = "PlayerStatusData", order = 0)]
 public class PlayerStatus : ScriptableObject
 {
     public float playerItemWeight;
     public float maxplayerWeight;
+    public int coin;
 
 }
